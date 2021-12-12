7d7117930b7f45a51eacfce1bb4ab7cf96b706fd
anzal1 <you@example.com>
2021-12-12 11:03:54 +0530

    change in profile picture

--- src/portfolio.js
+++ src/portfolio.js
@@ -543,7 +543,7 @@ const publicationsHeader = {
 const contactPageData = {
   contactSection: {
     title: "Contact Me",
-    profile_image_path: "anzaloriginal.png",
+    profile_image_path: "secondanzal.jpg",
     description:
       "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
   },
